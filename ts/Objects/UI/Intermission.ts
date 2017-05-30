c451eb8a97500d464f1a1e581e7d958fae811e3a
Boy Voesten <warrock.boy@hotmail.com>
2017-05-30 20:21:39 +0200

    Added TapToContinue text, polished star animation with screenshake.

--- ts/Objects/UI/Intermission.ts
+++ ts/Objects/UI/Intermission.ts
@@ -5,26 +5,30 @@
     export class Intermission extends Phaser.Group {
 
         public intermissionDone: Phaser.Signal;
-
+        // Clipboard
         private moveNormal: number;
-        private tween: Phaser.Tween;
+        private clipboardTween: Phaser.Tween;
         private clipboard: Phaser.Image;
         private screenOverlay: Phaser.Image;
-
+        // Text
+        private continueTextNormal: number;
+        private continueTextTween: Phaser.Tween;
+        private continueText: Phaser.Text;
         private speedBonusText: Phaser.Text;
         private accuracyBonusText: Phaser.Text;
         private speedBonus: number = 1;
         private accuracyBonus: number;
-
+        // Rating
         private starsAmount: number = 3;
         private starIsAnimating: boolean = false;
         private starTween: Phaser.Tween;
-        private starNorm: number;
+        private starNormal: number;
         private starScale: number = 0.6;
-        private starFull: Phaser.Image;
+        private starImgFull: Phaser.Image;
         private starsToPlace: Phaser.Image[];
         private ratingSpeedStars: Phaser.Image[];
         private ratingAccuracyStars: Phaser.Image[];
+        private shakeTimer: Phaser.TimerEvent;
 
         constructor(game: Phaser.Game) {
             super(game);
@@ -74,6 +78,23 @@
                 this.clipboard.width * 0.3, this.accuracyBonusText.height
             );
 
+            // Continue Text
+            this.continueText = new Phaser.Text(this.game, 0, 0, '~  Tap to continue  ~', {
+                font: '21pt Arial',
+                fontStyle: 'italic',
+                fill: '#000000',
+                boundsAlignH: 'middle',
+                boundsAlignV: 'middle'
+            });
+            this.continueText.anchor.setTo(0.5, 0.5);
+            this.continueText.alpha = 0;
+            this.clipboard.addChild(this.continueText);
+            this.continueText.setTextBounds(
+                this.clipboard.x,
+                this.clipboard.height / 2 * 0.5 / this.clipboard.scale.y,
+                this.continueText.width, this.continueText.height
+            );
+
             // Set empty star frames
             this.ratingSpeedStars = [];
             this.ratingAccuracyStars = [];
@@ -92,14 +113,7 @@
                 this.clipboard.addChild(ratingStarSpeed);
             }
 
-            // Full Star
-            this.starFull = new Phaser.Image(this.game, 0, 0, Images.RatingStarFull);
-            this.starFull.anchor.setTo(0.5, 0.5);
-            this.starFull.visible = false;
-            this.clipboard.addChild(this.starFull);
-            this.starsToPlace = [];
-
-            // Empty Stars
+            // Empty star frame positioning
             for (let i: number = 0; i < this.starsAmount; i++) {
                 let heightPercAcc: number = 0.155;
                 let heightPercSpeed: number = -0.145;
@@ -107,6 +121,13 @@
                 this.ratingAccuracyStars[i].y = -(this.clipboard.height / 2 * heightPercAcc) / this.clipboard.scale.y;
                 this.ratingSpeedStars[i].y = -(this.clipboard.height / 2 * heightPercSpeed) / this.clipboard.scale.y;
             }
+
+            // Full star
+            this.starImgFull = new Phaser.Image(this.game, 0, 0, Images.RatingStarFull);
+            this.starImgFull.anchor.setTo(0.5, 0.5);
+            this.starImgFull.visible = false;
+            this.clipboard.addChild(this.starImgFull);
+            this.starsToPlace = [];
 
             //this.openIntermission(new SessionData(0, 0, 0, 0, 0, 0, 0, 0));
             this.resize();
@@ -122,20 +143,32 @@
             this.accuracyBonus = session.calcAccuracyPerc();
 
             // Open the clipboard
+            if (this.clipboardTween && this.clipboardTween.isRunning) {
+                this.clipboardTween.stop();
+            }
             this.moveAnim = 0;
-            this.tween = this.game.add.tween(this).to({ moveAnim: 1 }, 1000, Phaser.Easing.Bounce.Out, true);
-            this.tween.onComplete.addOnce(this.onCompletedAnim, this, 0, session);
+            this.clipboardTween = this.game.add.tween(this).to({ moveAnim: 1 }, 1000, Phaser.Easing.Bounce.Out, true);
+            this.clipboardTween.onComplete.addOnce(this.onCompletedClipboardAnim, this, 0, session);
             this.clipboard.visible = true;
 
             // Set star frames
             this.showEmptyStars();
         }
 
-        private onCompletedAnim(): void {
+        private onCompletedClipboardAnim(): void {
             // Enable input overlay
             this.screenOverlay.visible = true;
             // Calc info needed for clipboard
             this.calculateRating();
+        }
+
+        private get continueTextAnim(): number {
+            return this.continueTextNormal;
+        }
+
+        private set continueTextAnim(value: number) {
+            this.continueTextNormal = value;
+            this.continueText.alpha = 0.30 + 0.40 * value;
         }
 
         private showEmptyStars(): void {
@@ -153,8 +186,8 @@
             this.screenOverlay.visible = false;
 
             // Close clipboard
-            this.tween = this.game.add.tween(this).to({ moveAnim: 0 }, 500, Phaser.Easing.Back.In, true);
-            this.tween.onComplete.addOnce(this.onIntermissionClosed, this);
+            this.clipboardTween = this.game.add.tween(this).to({ moveAnim: 0 }, 500, Phaser.Easing.Back.In, true);
+            this.clipboardTween.onComplete.addOnce(this.onIntermissionClosed, this);
         }
 
         private onIntermissionClosed(): void {
@@ -164,7 +197,7 @@
 
         private calculateRating(): void {
             let amountOfStars: number;
-            
+
             // Accuracy stars
             if (this.accuracyBonus >= 90) {
                 amountOfStars = 3;
@@ -191,62 +224,84 @@
                 this.starsToPlace.push(this.ratingSpeedStars[i]);
             }
 
-            this.placeStars();
+            this.placeAchievedStars();
         }
 
-        private placeStars(): void {
-            if (this.starsToPlace.length <= 0) {
-                return null;
-            }
-
+        /**
+         * Calls the animation for each achieved star (Loops till all placed)
+         */
+        private placeAchievedStars(): void {
+            // Call the animation
             if (this.starsToPlace.length > 0 && this.starIsAnimating === false) {
-                this.placeStar(this.starsToPlace[0], 0);
+                this.animateStarTowards(this.starsToPlace[0], 250);
             }
         }
 
-        private placeStar(spot: Phaser.Image, delay: number): void {
-            // Set star on spot
-            this.starFull.x = spot.x;
-            this.starFull.y = spot.y;
+        private animateStarTowards(spot: Phaser.Image, delay: number): void {
+            let animationSpeed: number = 350;
+
+            // Reset star
+            this.starImgFull.visible = false;
+            this.starImgFull.scale.x = this.starScale * 10;
+            this.starImgFull.scale.y = this.starImgFull.scale.x;
+            this.starImgFull.x = spot.x;
+            this.starImgFull.y = spot.y;
             this.starAnim = 1;
 
             if (this.starTween && this.starTween.isRunning) {
                 this.starTween.stop();
             }
 
+            // Tween star scale
             this.starIsAnimating = true;
-            this.starTween = this.game.add.tween(this).to({ starAnim: 0 }, 400, Phaser.Easing.Quintic.Out, true, delay);
-            this.starTween.onComplete.addOnce(() => { this.starTweenDone(spot) }, this);
-            this.starFull.visible = true;
+            this.starTween = this.game.add.tween(this).to({ starAnim: 0 }, animationSpeed, Phaser.Easing.Quintic.Out, true, delay);
+            this.starTween.onComplete.addOnce(() => { this.starTweenDone(spot); }, this);
+            this.starTween.onStart.addOnce(() => { this.starImgFull.visible = true; }, this);
+
+            // Timer for impact effects
+            if (this.shakeTimer) {
+                this.game.time.events.remove(this.shakeTimer);
+                this.shakeTimer = null;
+            }
+            this.shakeTimer = this.game.time.events.add(animationSpeed + 50, this.shakeTimedOut, this);
+        }
+
+        private shakeTimedOut(): void {
+            this.game.camera.shake(0.03, 30, true, Phaser.Camera.SHAKE_BOTH);
         }
 
         private get starAnim(): number {
-            return this.starNorm;
+            return this.starNormal;
         }
 
         private set starAnim(norm: number) {
-            this.starNorm = norm;
-            this.starFull.scale.x = this.starScale * (15 * norm + 1);
-            this.starFull.scale.y = this.starFull.scale.x;
+            this.starNormal = norm;
+            this.starImgFull.scale.x = this.starScale * (20 * norm + 1);
+            this.starImgFull.scale.y = this.starImgFull.scale.x;
         }
 
         private starTweenDone(spot: Phaser.Image): void {
-            console.log('starTweenDone');
-            this.game.camera.shake(0.025, 25, true, Phaser.Camera.SHAKE_BOTH);
             spot.loadTexture(Images.RatingStarFull);
             this.starsToPlace.splice(this.starsToPlace.indexOf(spot), 1);
             this.starIsAnimating = false;
 
-            // Reset and place next star
+            // Place next star if available
             if (this.starsToPlace.length > 0) {
-                console.log('starsToPlace > 0');
-                this.starFull.visible = false;
-                this.starFull.scale.x = this.starScale * 10;
-                this.starFull.scale.y = this.starFull.scale.x;
-                this.placeStar(this.starsToPlace[0], 0);
+                this.placeAchievedStars();
             } else {
-                this.screenOverlay.inputEnabled = true;
+                this.onDonePlacingStars();
             }
+        }
+
+        private onDonePlacingStars(): void {
+            // ContinueText tween
+            if (this.continueTextTween && this.continueTextTween.isRunning) {
+                this.continueTextTween.stop();
+            }
+            this.continueTextTween = this.game.add.tween(this).to({ continueTextAnim: 1 }, 1000, Phaser.Easing.Linear.None, true, 0, -1, true);
+
+            // Enable input to Continue
+            this.screenOverlay.inputEnabled = true;
         }
 
         private get moveAnim(): number {
@@ -269,7 +324,7 @@
         public resize(): void {
             this.clipboard.x = this.game.width / 2;
             this.clipboard.y = this.game.height / 2;
-            if (!this.tween || !this.tween.isRunning) {
+            if (!this.clipboardTween || !this.clipboardTween.isRunning) {
                 this.moveAnim = this.moveAnim;
             }
 
